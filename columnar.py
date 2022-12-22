59ad0ad6d24a0bee5db0045e7ca0ffdea92b8443
Ojas Karmarkar <ojask2002@gmail.com>
2022-12-22 22:38:02 +0530

    DONE

--- columnar.py
+++ columnar.py
@@ -1,5 +1,5 @@
 key = input("Enter the key : ")
-pt = input("Enter the pt : ").replace(' ' , ' ')
+pt = input("Enter the pt : ").replace(' ' , '')
 
 pt = list(pt)
 og = list(key)
@@ -13,6 +13,7 @@ for i in range(len(key)):
         order.append(og.index(s[i]))
         og[og.index(s[i])]="#"
 
+col_empty = []
 
 
 def encrypt(matrix , key , order):
@@ -25,11 +26,13 @@ def encrypt(matrix , key , order):
                 matrix[i].append('')
             k+=1
 
-
+    print(matrix)
     ct = []
     for i in range(len(order)):
         col_ix = order.index(i)
         for row_ix in range(rows):
+            if matrix[row_ix][col_ix]=='':
+                col_empty.append(col_ix)
             ct.append(matrix[row_ix][col_ix])
 
     ct=''.join(ct)
@@ -43,11 +46,23 @@ def decrypt(ct):
         k=0
         col_ix = order.index(i)
         while k<rows:
-            matrix[k][col_ix] = ct[str_ix]
-            str_ix+=1
-            k+=1
-        print(matrix)
+            if col_ix in col_empty and k==rows-1:
+                matrix[k][col_ix] = ''
+            else:
+                matrix[k][col_ix] = ct[str_ix]
+                str_ix+=1
 
+            k+=1
+
+    pt = []
+    for i in range(rows):
+            for j in range(len(key)):
+                pt.append(matrix[i][j])
+
+    pt=''.join(pt)
+    print("Decrypted text is : " + pt)
+    return pt
 
 ct = encrypt(matrix , key , order)
 pt = decrypt(ct)
+print(pt)
